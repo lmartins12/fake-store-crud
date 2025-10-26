28e9d9cde9424f0b3d63789c17f0ba738485a8a8
Lucas Martins <lucas.martins559@outlook.com>
2025-10-26 18:40:46 -0300

    feat(config): configura NgRx store e effects para gerenciamento de produtos
    - Integra NgRx store, effects e devtools na configuração da aplicação.
    - Configura providers relacionados a produtos na configuração de roteamento

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,11 @@
-import { ApplicationConfig, LOCALE_ID } from '@angular/core';
+import { ApplicationConfig, isDevMode, LOCALE_ID } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter, Routes } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { ConfirmationService, MessageService } from 'primeng/api';
+import { provideProductsEffects, provideProductsStore } from './features/products/store';
 
 export const routes: Routes = [
   {
@@ -11,6 +15,7 @@ export const routes: Routes = [
   },
   {
     path: 'products',
+    providers: [provideProductsStore(), provideProductsEffects()],
     loadComponent: () =>
       import('./features/products/products-list.page').then((m) => m.ProductsListPage),
   },
@@ -24,6 +29,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: !isDevMode(),
+    }),
     MessageService,
     ConfirmationService,
     { provide: LOCALE_ID, useValue: 'pt-BR' },
